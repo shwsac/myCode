85dd5aac85c5d61161df9a97d555f4092a9585d8
shweta-sachdeva <ss8740@nyu.edu>
2016-01-19 23:09:04 -0500

    Moving string problems in string folder.

---
+++ src/strings/WordPattern.java
@@ -0,0 +1,41 @@
+package strings;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class WordPattern {
+
+    public static boolean wordPattern(String pattern, String str) {
+        if(pattern == null && str == null) {
+            return true;
+        }
+        if(pattern == null || str == null) {
+            return false;
+        }
+        
+        char[] pat = pattern.toCharArray();
+        String[] tokens = str.split(" ");
+        if(tokens.length != pat.length) {
+            return false;
+        }
+        
+        HashMap<Character,String> hMap = new HashMap<Character,String>();
+        HashSet<String> tokenSet = new HashSet<String>();
+        for(int i = 0;i<pat.length;i++) {
+            if(hMap.containsKey(pat[i]) && hMap.get(pat[i]).equals(tokens[i])) {
+                continue;
+            } else if(!hMap.containsKey(pat[i]) && !tokenSet.contains(tokens[i])) {
+                hMap.put(pat[i], tokens[i]);
+                tokenSet.add(tokens[i]);
+            } else {
+                return false;
+            }           
+        }      
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordPattern("abba","dog cat cat fish"));
+
+    }
+
+}
